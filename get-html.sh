cc59c3d7a3091c7a1830b6d961d9807e005c51b6
Daryl.Xu <ziqiang_xu@yeah.net>
2019-06-19 19:54:40 +0800

    Update script

--- get-html.sh
+++ get-html.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 rm docs/* -rf
-mv ../ziqiangxu.github.io/docs/.vuepress/dist/* docs/
+cp -r ../ziqiangxu.github.io/docs/.vuepress/dist/* docs/
 git add docs/
 git commit -m "Update posts"
