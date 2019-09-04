694add565887e2fd0389c733ff3856a471979dc0
Daryl Xu <ziqiang_xu@yeah.net>
2019-09-04 13:43:22 +0800

    Update script

--- update-post.sh
+++ update-post.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 git pull
 rm docs/* -rf
+cd ../ziqiangxu.github.io/
+npm run docs:build
+cd -
 cp -r ../ziqiangxu.github.io/docs/.vuepress/dist/* docs/
 git add docs/
 git commit -m "Update posts"
