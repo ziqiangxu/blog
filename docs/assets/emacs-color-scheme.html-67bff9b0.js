3bf5b917b83d452a81b9044bc6597c0ecb2c0c29
Daryl <ziqiang_xu@qq.com>
2025-10-20 23:05:11 +0800

    Update posts

---
+++ docs/assets/emacs-color-scheme.html-67bff9b0.js
@@ -0,0 +1,4 @@
+import{_ as e,o as a,c as s,e as c}from"./app-2772a4f0.js";const n={},d=c(`<h1 id="emacs-背景和文字颜色配置" tabindex="-1"><a class="header-anchor" href="#emacs-背景和文字颜色配置" aria-hidden="true">#</a> <code>emacs</code> 背景和文字颜色配置</h1><div class="language-bash line-numbers-mode" data-ext="sh"><pre class="language-bash"><code>M-x
+customize-face <span class="token comment"># 回车</span>
+default <span class="token comment"># 回车</span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>然后就进入了配置页面</p>`,3),o=[d];function t(i,l){return a(),s("div",null,o)}const m=e(n,[["render",t],["__file","emacs-color-scheme.html.vue"]]);export{m as default};
